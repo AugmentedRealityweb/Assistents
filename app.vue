dec6e2180c2a68141c021e22078845051782ad5b
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-12-24 18:49:23 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -252,14 +252,14 @@ export default {
 
 .description {
   position: fixed;
-  bottom: 40px;
+  bottom: 60px;
   left: 50%;
   transform: translateX(-50%);
   background: rgba(0, 0, 0, 0.8);
   color: white;
   padding: 10px;
   border-radius: 10px;
-  max-width: 80%;
+  max-width: 100%;
   text-align: center;
   font-size: 1rem;
 }
