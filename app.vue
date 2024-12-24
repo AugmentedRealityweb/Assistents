66fcc9fd0b95f0d45ec268fd96c0346de6c3de22
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-12-24 18:07:57 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -234,8 +234,13 @@ export default {
   color: white;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-  max-width: 180%;
+  max-width: 80%;
   margin: 0 auto;
   font-size: 1.2rem;
+  position: fixed;
+  transform: translate(-50%, -50%);
+  top: 70%; /* Sau valoarea dorită */
+  left: 50%; /* Sau valoarea dorită */
+}
 }
 </style>
