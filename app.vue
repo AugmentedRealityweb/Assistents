d1b35bac6c08edd8c191f3e802ca27081f36f8e3
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-12-25 21:23:31 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -129,7 +129,6 @@ export default {
         localStorage.removeItem("sessionId");
       }
     },
-    methods: {
     validatePaymentTime() {
       const paymentTimestamp = localStorage.getItem("paymentTimestamp");
       if (paymentTimestamp) {
